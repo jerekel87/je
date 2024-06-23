bd69507c6199c3d99467c41734605f9bf6d6e4cb
jay philip <jayphilipcamillo@gmail.com>
2024-06-23 14:08:37 +0800

    .

--- app/home/components/ProjectMarquee.tsx
+++ app/home/components/ProjectMarquee.tsx
@@ -4,47 +4,17 @@ import React from "react";
 import { Project } from "@/sanity.types";
 import Marquee from "react-fast-marquee";
 import ProjectItem from "./ProjectItem";
-import { AspectRatio } from "@/app/(shared)/components/ui/aspect-ratio";
 
 function ProjectMarquee({ projects }: { projects: Project[] }) {
   return (
-    // <Swiper
-    //   loop={true}
-    //   slidesPerView={1.5}
-    //   spaceBetween={0}
-    //   speed={2500}
-    //   autoplay={{ delay: 0 }}
-    //   modules={[FreeMode, Autoplay]}
-    //   style={{
-    //     // @ts-ignore
-    //     "--swiper-wrapper-transition-timing-function": "linear",
-    //   }}
-    //   breakpoints={{
-    //     640: {
-    //       slidesPerView: 2,
-    //     },
-    //     768: {
-    //       slidesPerView: 4,
-    //     },
-    //     1024: {
-    //       slidesPerView: 6,
-    //     },
-    //   }}
-    // >
-    //   {projects.map((project, i) => (
-    //     <SwiperSlide key={i}>
-    //       <ProjectItem key={project._id} project={project} />
-    //     </SwiperSlide>
-    //   ))}
-    // </Swiper>
     <Marquee
       loop={0}
       pauseOnClick
       pauseOnHover
       autoFill
-      className="max-w-[2560px] bg-red-400 mx-auto"
+      className="max-w-[2560px] mx-auto"
     >
-      <div className="w-screen max-w-[2560px] bg-orange-400 flex">
+      <div className="w-screen max-w-[2560px] flex">
         {projects.map((project) => (
           <div key={project._id} className="w-1/2 md:w-1/4 lg:w-1/6 shrink-0">
             <ProjectItem key={project._id} project={project} />
