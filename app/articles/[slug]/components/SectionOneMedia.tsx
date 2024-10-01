aa82b6b0f912d28637ae939b4d410e2cfbc25fa9
jay philip <jayphilipcamillo@gmail.com>
2024-10-01 18:22:01 +0800

    floating share button

--- app/articles/[slug]/components/SectionOneMedia.tsx
+++ app/articles/[slug]/components/SectionOneMedia.tsx
@@ -8,12 +8,7 @@ import { urlForImage } from "@/sanity/lib/image";
 
 function SectionOneMedia({ article }: { article: Article }) {
   return (
-    <div className="relative mt-6 lg:mt-[58px]">
-      <div className="hidden lg:flex absolute -right-[100px] top-[50px] z-[1] flex-col items-center">
-        <ShareModal pathname={`articles/${article.slug?.current}`} />
-        <p className="mt-[10px] text-sm font-bold">Share</p>
-      </div>
-
+    <div className="relative">
       <div className="w-full h-0 pb-[75.8%] relative">
         <div className="w-full h-0 pb-[75.8%] relative rounded-[6px] lg:rounded-[10px] bg-black overflow-hidden">
           {article.mainVideoLink ? (
--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -4,7 +4,9 @@ import ArticleSection from "./components/ArticleSection";
 import SectionFour from "./components/SectionFour";
 import Header from "@/app/(shared)/components/Header";
 import FakePurchase from "@/app/(shared)/components/fake-purchase/FakePurchase";
+import ShareModalContainer from "./components/ShareModalContainer";
 import { getArticle } from "@/sanity/query/article";
+import SectionOneMedia from "./components/SectionOneMedia";
 
 export const revalidate = 60;
 
@@ -17,28 +19,24 @@ async function ArticlePage({ params }: { params: { slug: string } }) {
     <>
       <Header />
       <div className="border-[1px] border-muted shadow-sm" />
-      <main className="container px-3 max-w-[900px] pb-[100px] lg:pb-[152px] pt-[40px] lg:pt-[80px]">
-        {/* Floating button */}
-        {/* <div className="hidden lg:block sticky z-[1] top-0 max-w-[900px] mx-auto">
-          <div className="absolute -right-[100px] top-[50svh] -translate-y-1/2 flex flex-col gap-[24px]">
-            <div className="flex flex-col items-center">
-              <ShareModal pathname={`articles/${article.slug?.current}`} />
-              <p className="mt-[10px] text-sm font-bold">Share</p>
-            </div>
-          </div>
-        </div> */}
-
+      <main className="container px-3 lg:max-w-[1146px] pb-[100px] lg:pb-[152px] pt-[40px] lg:pt-[80px]">
         <SectionOne article={article} />
+        <div className="lg:flex lg:gap-[35px] lg:pl-[117px] mt-6 lg:mt-[58px]">
+          <div>
+            <SectionOneMedia article={article} />
 
-        {(article.block?.title || article.block?.body) && (
-          <SectionTwo article={article} />
-        )}
+            {(article.block?.title || article.block?.body) && (
+              <SectionTwo article={article} />
+            )}
 
-        {article.sections?.map((articleSection: any, i: number) => (
-          <ArticleSection key={i} articleSection={articleSection} />
-        ))}
+            {article.sections?.map((articleSection: any, i: number) => (
+              <ArticleSection key={i} articleSection={articleSection} />
+            ))}
 
-        <SectionFour />
+            <SectionFour />
+          </div>
+          <ShareModalContainer pathname={`articles/${article.slug?.current}`} />
+        </div>
       </main>
       <div className="hidden lg:block">
         <FakePurchase />
