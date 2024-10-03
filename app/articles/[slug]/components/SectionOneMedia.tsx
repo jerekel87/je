e9e3f59a0130a3c5dfe1039bedd0f500a7585ed2
jay philip <jayphilipcamillo@gmail.com>
2024-10-03 19:32:28 +0800

    .

--- app/articles/[slug]/components/SectionOneMedia.tsx
+++ app/articles/[slug]/components/SectionOneMedia.tsx
@@ -2,34 +2,29 @@
 
 import Image from "next/image";
 import ReactPlayer from "react-player";
-import ShareModal from "@/app/(shared)/components/ShareModal";
 import { Article } from "@/sanity.types";
 import { urlForImage } from "@/sanity/lib/image";
 
 function SectionOneMedia({ article }: { article: Article }) {
   return (
-    <div className="relative">
-      <div className="w-full h-0 pb-[75.8%] relative">
-        <div className="w-full h-0 pb-[75.8%] relative rounded-[6px] lg:rounded-[10px] bg-black overflow-hidden">
-          {article.mainVideoLink ? (
-            <div className="w-full pb-[56.4%] absolute top-1/2 -translate-y-1/2">
-              <ReactPlayer
-                width="100%"
-                height="100%"
-                style={{ position: "absolute", objectFit: "contain" }}
-                url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
-              />
-            </div>
-          ) : (
-            <Image
-              src={urlForImage(article.mainImage as any)}
-              alt={article.title || ""}
-              fill
-              className="object-cover"
-            />
-          )}
+    <div className="w-[100vw] -left-4 lg:left-0 lg:w-full h-0 pb-[75.8%] relative lg:rounded-[10px] bg-black overflow-hidden">
+      {article.mainVideoLink ? (
+        <div className="w-full pb-[56.4%] absolute top-1/2 -translate-y-1/2">
+          <ReactPlayer
+            width="100%"
+            height="100%"
+            style={{ position: "absolute", objectFit: "contain" }}
+            url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+          />
         </div>
-      </div>
+      ) : (
+        <Image
+          src={urlForImage(article.mainImage as any)}
+          alt={article.title || ""}
+          fill
+          className="object-cover"
+        />
+      )}
     </div>
   );
 }
