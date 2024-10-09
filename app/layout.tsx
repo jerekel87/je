dd2c26c506395fbe33899cb79715bb40ab3a764a
jay philip <jayphilipcamillo@gmail.com>
2024-10-09 16:58:21 +0800

    change pixel id

--- app/layout.tsx
+++ app/layout.tsx
@@ -108,7 +108,7 @@ export default function RootLayout({
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '5475878092436477');
+          fbq('init', '1501275917193892');
           fbq('track', 'PageView');
         `}
         </Script>
@@ -117,7 +117,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=5475878092436477&ev=PageView&noscript=1`}
+            src={`https://www.facebook.com/tr?id=1501275917193892&ev=PageView&noscript=1`}
           />
         </noscript>
       </body>
