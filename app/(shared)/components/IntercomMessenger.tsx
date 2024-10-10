63d1936068988094fd5f9e171bec9fdc4c3b2291
jay philip <jayphilipcamillo@gmail.com>
2024-10-10 17:37:37 +0800

    .

--- app/(shared)/components/IntercomMessenger.tsx
+++ app/(shared)/components/IntercomMessenger.tsx
@@ -5,31 +5,26 @@ import { Intercom as IntercomSDK } from "@intercom/messenger-js-sdk";
 
 function IntercomMessenger(): React.ReactElement | null {
   useEffect(() => {
+    // Initialize Intercom using a factory function pattern
     IntercomSDK({
       app_id: "r8e9gnqq",
     });
 
-    const toggleIntercomOnDevice = () => {
-      const isMobile = window.innerWidth <= 768; // Define your breakpoint for mobile
+    // // Load the Intercom messenger
+    // intercom.boot();
 
-      if (isMobile) {
-        window.Intercom && window.Intercom("hide");
-      } else {
-        window.Intercom && window.Intercom("show");
-      }
-    };
+    // // Update Intercom with user information (if available)
+    // const userSettings: Partial<IntercomSettings> = {
+    //   email: "user@example.com",
+    //   user_id: "123",
+    //   name: "John Doe",
+    // };
+    // intercom.update(userSettings);
 
-    // Initial call to set the correct visibility
-    toggleIntercomOnDevice();
-
-    // Add resize event listener to toggle visibility based on screen size
-    window.addEventListener("resize", toggleIntercomOnDevice);
-
-    // Cleanup function to remove the event listener on unmount
-    return () => {
-      window.removeEventListener("resize", toggleIntercomOnDevice);
-      window.Intercom && window.Intercom("shutdown"); // Shutdown Intercom when the component unmounts
-    };
+    // Clean up on component unmount
+    // return () => {
+    //   intercom.shutdown();
+    // };
   }, []);
 
   return null; // This component doesn't render any UI
