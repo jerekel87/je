b4d5957e7d601c0b3c69b39f32fcc8fcf3c9e398
jay philip <jayphilipcamillo@gmail.com>
2024-10-29 19:35:45 +0800

    fixed footer top bg

--- app/(shared)/components/FooterTopBg.tsx
+++ app/(shared)/components/FooterTopBg.tsx
@@ -2,21 +2,23 @@
 
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { cn } from "../lib/utils";
 
 function FooterTopBg() {
   const pathname = usePathname();
   const isFAQPage = pathname.includes("/faq");
   const isArticlesPage = pathname.includes("/articles");
   const isPricingPage = pathname.includes("/pricing");
-  const topBgColor =
-    isFAQPage || isArticlesPage || isPricingPage ? "#fff" : "#f9f8f3";
+  const isTopBgColorWhite = isFAQPage || isArticlesPage || isPricingPage;
   return (
     <div
-      className="h-[135px] lg:h-[235px] w-full relative static-background"
-      style={{ backgroundColor: topBgColor }}
+      className={cn(
+        "h-[135px] lg:h-[235px] w-full relative",
+        !isTopBgColorWhite && "static-background-hard"
+      )}
     >
       <div className="absolute w-full h-[27px] lg:h-[37px] -bottom-[6px] z-[2]">
-        {topBgColor === "#f9f8f3" ? (
+        {!isTopBgColorWhite ? (
           <Image
             src="/assets/images/shape-7-copy-14.svg"
             fill
